e792fc18ddc17fd478109005aef8e94f9d391502
manzo94 <andrea.manzini1994@gmail.com>
2017-12-10 23:41:00 +0100

    Added KNNBaseline method

--- Recommender System/code/surprise_helper.py
+++ Recommender System/code/surprise_helper.py
@@ -1,5 +1,6 @@
-from surprise import Dataset, BaselineOnly, evaluate, print_perf, Reader
+from surprise import Dataset, KNNBaseline, BaselineOnly, evaluate, print_perf, Reader
 from format_helper import export_prediction
+import pandas as pd
 
 def pred_final_BaselineOnly(data, test, bsl_options):
     """ Find prediction for BaselineOnly method using entire trainset
@@ -7,6 +8,16 @@ def pred_final_BaselineOnly(data, test, bsl_options):
     
     trainset = data.build_full_trainset()
     algo = BaselineOnly(bsl_options=bsl_options)
+    algo.train(trainset)
+    prediction = algo.test(test)
+    export_prediction(prediction)
+    
+def pred_final_KNNBaseline(data, test, k=40, min_k=1, sim_options={}, bsl_options={}):
+    """ Find prediction for BaselineOnly method using entire trainset
+        and export them in the kaggle format"""
+    
+    trainset = data.build_full_trainset()
+    algo = KNNBaseline(k, min_k, sim_options, bsl_options)
     algo.train(trainset)
     prediction = algo.test(test)
     export_prediction(prediction)
@@ -20,3 +31,8 @@ def import_kaggle_testset(path_samplesub):
     # Build testset based on the ratings in the sample
     test = test_data.build_full_trainset().build_testset()
     return test
+
+def save_gridsearch_info(grid_search, file_name):
+    # Use Panda dataframe to analyze solution
+    results_df = pd.DataFrame.from_dict(grid_search.cv_results)
+    results_df.to_csv('../method_reports/' + file_name)
--- Recommender System/code/surprise_test.py
+++ Recommender System/code/surprise_test.py
@@ -17,9 +17,15 @@ data = Dataset.load_from_file(file_path, reader=reader)
 random.seed(10)
 data.split(n_folds=4)  # data can now be used normally
 
+#%% GENERAL PARAMETERS SETTING
 
+bsl_options = { 'method': 'als',
+                'reg_i': 1.e-5,
+                'reg_u': 14.6,
+                'n_epochs': 10
+               }
 
-#%%
+#%% GRID SEARCH OF PARAMETERS
 
 # Set Grid Parameters
 param_grid = {
@@ -27,34 +33,29 @@ param_grid = {
                 'method': ['als'],
                 'reg_i': [1.e-5],
                 'reg_u': [14.6],
-                'n_epochs': [10]
+                'n_epochs': [5]
                 },
         
         'sim_options': {
                 'name': ['pearson_baseline'],
-                'shrinkage': [0,5,10]  # no shrinkage
+                'shrinkage': [100]
                 },
         
-        'k' : [30]
+        'k' : [300]
 }
 
+# Init grid_search
 grid_search = GridSearch(KNNBaseline, param_grid, measures=['RMSE'])
 
 # Evaluate performances of our algorithm on the dataset.
 grid_search.evaluate(data)
 
-# Use Panda dataframe to analyze solution
-results_df = pd.DataFrame.from_dict(grid_search.cv_results)
-results_df.to_csv('../method_reports/KNNBaseline.csv')
-print(results_df)
+# Print best score and best parameters
+print('Best Score: ', grid_search.best_score)
+print('Best parameters: ', grid_search.best_params)
 
-#%%
+#%% EVALUATE SINGLE ALGORITHM
 """
-bsl_options = { 'method': 'als',
-                'reg_i': 1.e-5,
-                'reg_u': 14.6,
-                'n_epochs': 10
-               }
 algo = BaselineOnly(bsl_options=bsl_options)
 rmse_tr = np.zeros(data.n_folds)
 rmse_te = np.zeros(data.n_folds)
@@ -75,14 +76,19 @@ for trainset, testset in data.folds():
 print('Train score: ', np.mean(rmse_tr))
 print('Test Score: ', np.mean(rmse_te))
 """
-#%% Write Final predictions
-"""
+#%% PREDICT AND WRITE KAGGLE PREDICTIONS
+
 bsl_options = { 'method': 'als',
                 'reg_i': 1.e-5,
                 'reg_u': 14.6,
                 'n_epochs': 10
                }
+sim_options = {
+                'name': 'pearson_baseline',
+                'shrinkage': 100
+                }
+k = 300
+min_k = 10
 
 test = import_kaggle_testset('../data/sample_formatted.csv')
-pred_final_BaselineOnly(data, test, bsl_options)
-"""
+pred_final_KNNBaseline(data, test, k, min_k, sim_options, bsl_options)
